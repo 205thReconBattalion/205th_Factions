366cc674f3ef54d5f3fac1352c6c93a8be002580
Spark2000 <paulalex.wetzel@gmail.com>
2025-08-06 18:04:59 +0200

    add: Heroes And Villains

---
+++ addons/RB205_heroesAndVillains/config.cpp
@@ -0,0 +1,79 @@
+class cfgPatches
+{
+	class RB205_factions_heroesAndVillains
+	{
+		requiredAddons[] = {};
+		requiredVersion = 1.0;
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class CfgFactionClasses
+{
+	class RB205_lore_heroes
+	{
+		displayName = "[205] Heroes of the Galaxy";
+		priority = 999;
+		side = 1;
+	};
+	class RB205_lore_villains
+	{
+		displayName = "[205] Villains of the Galaxy";
+		priority = 999;
+		side = 0;
+	};
+};
+class CfgEditorSubcategories
+{
+	class RB205_lore_jedi
+	{
+		displayName = "Jedi";
+	};
+	class RB205_lore_sith
+	{
+		displayName = "Sith";
+	};
+	class RB205_lore_other
+	{
+		displayName = "Other";
+	};
+};
+
+#include "macros.hpp"
+
+class CfgVehicles
+{
+	class RB205_unit_base;
+	class RB205_factions_hav_base: RB205_unit_base
+	{
+		ACCESS_FALSE
+		uniformClass = "RB205_factions_hav_U_base";
+		side = 1;
+		faction = "RB205_lore_heroes";
+		editorSubCategory = "RB205_lore_other";
+		identityTypes[] = {"LanguageENG_F","WhiteHead_11"};
+		//Inventory:
+		backpack = "";
+		weapons[] = {"Throw","Put"};
+		respawnweapons[] = {"Throw","Put"};
+	};
+};
+class CfgWeapons
+{
+	class help_uniform_base;
+	class RB205_uniform_base: help_uniform_base
+	{
+		class ItemInfo;
+	};
+	class RB205_factions_hav_U_base: RB205_uniform_base
+	{
+		ACCESS_FALSE
+		//picture = "\RB205_main\data\ui\uniforms\icon_U_default.paa";
+		//model = "\ls\core\addons\characters_clone_legacy\uniforms\phase2\ls_gar_phase2_uniform.p3d";
+		class ItemInfo: ItemInfo
+		{
+			uniformClass = "RB205_factions_hav_base";
+		};
+	};
+};
