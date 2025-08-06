1566498ce299aa7e73a15ecd95207e39e849ac84
Spark2000 <paulalex.wetzel@gmail.com>
2025-08-07 00:49:11 +0200

    add: Wookies

---
+++ addons/RB205_wookie/hideCfgWeapons.hpp
@@ -0,0 +1,21 @@
+class Uniform_Base;
+class ItemCore;
+class Vest_Camo_Base;
+
+class SWOP_Reb_f_WW_Chewbacca1_CombatUniform: Uniform_Base { ACCESS_FALSE };
+class SWOP_Reb_f_WW_Chewbacca3_CombatUniform: Uniform_Base { ACCESS_FALSE };
+class SWOP_Reb_f_WW_Chewbacca5_CombatUniform: Uniform_Base { ACCESS_FALSE };
+
+class SWOP_Helmetwookiechief: ItemCore { ACCESS_FALSE };//
+class SWOP_Helmetwookieskull: ItemCore { ACCESS_FALSE };//
+
+class SWOP_Vestwookie: Vest_Camo_Base { ACCESS_FALSE };//
+class SWOP_Vestwookie2: Vest_Camo_Base { ACCESS_FALSE };//
+class SWOP_Vestwookie3: Vest_Camo_Base { ACCESS_FALSE };//
+class SWOP_Vestwookie4: Vest_Camo_Base { ACCESS_FALSE };
+class SWOP_Vestwookie6: Vest_Camo_Base { ACCESS_FALSE };
+class SWOP_Vestwookie7: Vest_Camo_Base { ACCESS_FALSE };
+class SWOP_Vestwookie8: Vest_Camo_Base { ACCESS_FALSE };
+class SWOP_Vestwookie9: Vest_Camo_Base { ACCESS_FALSE };//
+class SWOP_Vestwookie10: Vest_Camo_Base { ACCESS_FALSE };
+class SWOP_Vestwookie11: Vest_Camo_Base { ACCESS_FALSE };
