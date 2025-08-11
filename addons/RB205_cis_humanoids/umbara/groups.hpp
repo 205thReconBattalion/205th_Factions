9929dc5c0c4ce2c1771562877598c7b0f861be6c
Spark2000 <paulalex.wetzel@gmail.com>
2025-08-11 20:22:56 +0200

    add: Umbaran

---
+++ addons/RB205_cis_humanoids/umbara/groups.hpp
@@ -0,0 +1,119 @@
+class RB205_umbaran_patrol
+{
+    name = "[2] Patrol";
+    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+    side = 0;
+    faction = "RB205_cis_humanoids";
+    class Unit0
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "CORPORAL";
+        position[] = {0,0,0};
+    };
+    class Unit1
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-2,0};
+    };
+};
+class RB205_umbaran_fireteam
+{
+    name = "[4] Fireteam";
+    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+    side = 0;
+    faction = "RB205_cis_humanoids";
+    class Unit0
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "SERGEANT";
+        position[] = {0,0,0};
+    };
+    class Unit1
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-1.5,0};
+    };
+    class Unit2
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-3,0};
+    };
+    class Unit3
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-4.5,0};
+    };
+};
+class RB205_umbaran_squad
+{
+    name = "[8] Squad";
+    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+    side = 0;
+    faction = "RB205_cis_humanoids";
+    class Unit0
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "SERGEANT";
+        position[] = {0,0,0};
+    };
+    class Unit1
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-1.5,0};
+    };
+    class Unit2
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-3,0};
+    };
+    class Unit3
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-4.5,0};
+    };
+    class Unit4
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-6,0};
+    };
+    class Unit5
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-7.5,0};
+    };
+    class Unit6
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-9,0};
+    };
+    class Unit7
+    {
+        side = 0;
+        vehicle = "RB205_umbaran";
+        rank = "PRIVATE";
+        position[] = {0,-10.5,0};
+    };
+};
