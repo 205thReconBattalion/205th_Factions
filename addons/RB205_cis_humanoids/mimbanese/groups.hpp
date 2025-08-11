c1bcf22ba13581180181eae8b7702e427f91696a
Spark2000 <paulalex.wetzel@gmail.com>
2025-08-11 20:23:27 +0200

    add: Mimbanese

---
+++ addons/RB205_cis_humanoids/mimbanese/groups.hpp
@@ -0,0 +1,175 @@
+class RB205_mimbaneseRebel_patrol
+{
+    name = "[2] Patrol";
+    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+    side = 0;
+    faction = "RB205_cis_humanoids";
+    class Unit0
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "CORPORAL";
+        position[] = {0,0,0};
+    };
+    class Unit1
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "PRIVATE";
+        position[] = {0,-2,0};
+    };
+};
+class RB205_mimbaneseRebel_patrol_sniper
+{
+    name = "[2] Patrol (Sniper)";
+    icon = "\A3\ui_f\data\map\markers\nato\o_recon.paa";
+    side = 0;
+    faction = "RB205_cis_humanoids";
+    class Unit0
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel_sniper";
+        rank = "CORPORAL";
+        position[] = {0,0,0};
+    };
+    class Unit1
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel_sniper";
+        rank = "PRIVATE";
+        position[] = {0,-2,0};
+    };
+};
+class RB205_mimbaneseRebel_fireteam
+{
+    name = "[4] Fireteam";
+    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+    side = 0;
+    faction = "RB205_cis_humanoids";
+    class Unit0
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "SERGEANT";
+        position[] = {0,0,0};
+    };
+    class Unit1
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel_heavy";
+        rank = "PRIVATE";
+        position[] = {0,-1.5,0};
+    };
+    class Unit2
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "PRIVATE";
+        position[] = {0,-3,0};
+    };
+    class Unit3
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "PRIVATE";
+        position[] = {0,-4.5,0};
+    };
+};
+class RB205_mimbaneseRebel_fireteam_at
+{
+    name = "[4] Fireteam (Anti-Tank)";
+    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+    side = 0;
+    faction = "RB205_cis_humanoids";
+    class Unit0
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "SERGEANT";
+        position[] = {0,0,0};
+    };
+    class Unit1
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel_at";
+        rank = "PRIVATE";
+        position[] = {0,-1.5,0};
+    };
+    class Unit2
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel_at";
+        rank = "PRIVATE";
+        position[] = {0,-3,0};
+    };
+    class Unit3
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "PRIVATE";
+        position[] = {0,-4.5,0};
+    };
+};
+class RB205_mimbaneseRebel_squad
+{
+    name = "[8] Squad";
+    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+    side = 0;
+    faction = "RB205_cis_humanoids";
+    class Unit0
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "SERGEANT";
+        position[] = {0,0,0};
+    };
+    class Unit1
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "PRIVATE";
+        position[] = {0,-1.5,0};
+    };
+    class Unit2
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "PRIVATE";
+        position[] = {0,-3,0};
+    };
+    class Unit3
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "PRIVATE";
+        position[] = {0,-4.5,0};
+    };
+    class Unit4
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel_heavy";
+        rank = "PRIVATE";
+        position[] = {0,-6,0};
+    };
+    class Unit5
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel";
+        rank = "PRIVATE";
+        position[] = {0,-7.5,0};
+    };
+    class Unit6
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel_demo";
+        rank = "PRIVATE";
+        position[] = {0,-9,0};
+    };
+    class Unit7
+    {
+        side = 0;
+        vehicle = "RB205_mimbaneseRebel_sniper";
+        rank = "PRIVATE";
+        position[] = {0,-10.5,0};
+    };
+};
