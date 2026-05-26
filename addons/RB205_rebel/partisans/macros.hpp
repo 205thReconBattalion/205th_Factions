25c162bd0fb145f74efae564969206474e014d0d
Potzga <siegfried-71@gmx.de>
2026-05-26 22:03:26 +0200

    Anpassung der Macros/Ueberfuehrung aus CIS Droiden

--- addons/RB205_rebel/partisans/macros.hpp
+++ addons/RB205_rebel/partisans/macros.hpp
@@ -1,12 +1,28 @@
-#define NO_SIDE -1
-#define EAST 0			// (Russian)
-#define WEST 1			// (NATO)
-#define RESISTANCE 2	// Guerilla
-#define CIVILIAN 3
-#define NEUTRAL 4
-#define ENEMY 5
-#define FRIENDLY 6
-#define LOGIC 7
+#include "\RB205_core\macros.hpp"
 
 #define INDIPENDENT_ID_CARD "RB205_ids_neutral_civ"
 #define PACKING_BANDAGE "ACE_packingBandage"
+
+//macros for weapons and magazines
+#define WEAPON_PRIMARY 	"RB205_E5"
+#define MAG_PRIMARY	"RB205_E5_mag"
+
+#define WEAPON_HEAVY 	"RB205_E5C"
+#define MAG_HEAVY	"RB205_E5C_mag"
+
+#define WEAPON_SNIPER 	"RB205_E5S"
+#define MAG_SNIPER 	"RB205_E5S_mag"
+
+#define WEAPON_AT	"RB205_RPS6_CIS"
+#define WEAPON_AA	"RB205_E60R"
+#define MAG_AT 		"RB205_RPS6_AT_mag"
+#define MAG_AA 		"RB205_E60R_AA_mag"
+#define MAG_TP_AT 	RB205_RPS6_AT_mag
+#define MAG_TP_AA 	RB205_E60R_AA_mag
+
+//macros for unit icons
+#define ICON_DEFAULT	icon = "iconMan";
+#define ICON_OFFICER	icon = "iconManOfficer";
+#define ICON_HEAVY		icon = "JLTS_iconManSupportGunner";
+#define ICON_SNIPER		icon = "JLTS_iconManSniper";
+#define ICON_AT 		icon = "iconManAT";
