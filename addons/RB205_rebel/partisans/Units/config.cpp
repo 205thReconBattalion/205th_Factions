7f57fc073e848bec7d553891b6a92d8f91364aa8
Potzga <siegfried-71@gmx.de>
2026-05-26 22:03:50 +0200

    Erweiterung der Rebelen

--- addons/RB205_rebel/partisans/Units/config.cpp
+++ addons/RB205_rebel/partisans/Units/config.cpp
@@ -12,13 +12,15 @@ class CfgPatches
 			"RB205_partisan_squadLead",
 			"RB205_partisan_fighter",
 			"RB205_partisan_fighterAT",
-			"RB205_partisan_fighterAA"
+			"RB205_partisan_fighterAA",
+			"RB205_partisan_medic",
+			"RB205_partisan_fighter_heavy",
+			"RB205_partisan_fighter_sniper"
 			};
 		weapons[] = {};
 	};
 };
 
-#include "\RB205_cis\droids\macros.hpp"
 #include "\RB205_rebel\partisans\macros.hpp"
 
 class CfgVehicles
@@ -31,16 +33,16 @@ class CfgVehicles
 		side = 2;
 		displayName = "Squad Lead";
 		ICON_DEFAULT
-		weapons[]=          { WEAPON_E5, "Throw", "Put" };
-		respawnWeapons[]=   { WEAPON_E5, "Throw", "Put" };
-		magazines[]=        { MAG_8(MAG_E5)};
-		respawnMagazines[]= { MAG_8(MAG_E5)};
+		weapons[]=          { WEAPON_PRIMARY, "Throw", "Put" };
+		respawnWeapons[]=   { WEAPON_PRIMARY, "Throw", "Put" };
+		magazines[]=        { ENUM_8(MAG_PRIMARY) };
+		respawnMagazines[]= { ENUM_8(MAG_PRIMARY) };
 		faction = "RB205_rebel";
 		editorSubcategory = "RB205_rebel_partisans";
 		linkedItems[]= {"JMSLLTE_InvVest_armor", "JMSLLTE_Commando_saw_helmet","ItemMap", "ItemCompass", "ItemWatch", "ItemRadio"};
 		respawnlinkedItems[]=   {"JMSLLTE_InvVest_armor", "JMSLLTE_Commando_saw_helmet","ItemMap", "ItemCompass", "ItemWatch", "ItemRadio"};
-		items[]=            {INDIPENDENT_ID_CARD, MAG_10(PACKING_BANDAGE)};
-		respawnItems[]=     {INDIPENDENT_ID_CARD, MAG_10(PACKING_BANDAGE)};
+		items[]=            {INDIPENDENT_ID_CARD, ENUM_10(PACKING_BANDAGE)};
+		respawnItems[]=     {INDIPENDENT_ID_CARD, ENUM_10(PACKING_BANDAGE)};
 	};
 
 	class RB205_partisan_fighter : RB205_partisan_squadLead
@@ -53,22 +55,22 @@ class CfgVehicles
 	{
 		displayName = "Fighter (AT)";
 		uniformClass = "JMSLLTE_ScumPart_br_F_CombatUniform";
-		weapons[]=          { WEAPON_E5, WEAPON_AT, "Throw", "Put" };
-		respawnWeapons[]=   { WEAPON_E5, WEAPON_AT, "Throw", "Put" };
+		weapons[]=          { WEAPON_PRIMARY, WEAPON_AT, "Throw", "Put" };
+		respawnWeapons[]=   { WEAPON_PRIMARY, WEAPON_AT, "Throw", "Put" };
 		backpack = "RB205_partisan_fighter_backpack_AT";
-		magazines[]=        { MAG_8(MAG_E5), MAG_AT};
-		respawnMagazines[]= { MAG_8(MAG_E5), MAG_AT};
+		magazines[]=        { ENUM_8(MAG_PRIMARY), MAG_AT};
+		respawnMagazines[]= { ENUM_8(MAG_PRIMARY), MAG_AT};
 	};
 
-	class RB205_partisan_fighter_AA : RB205_partisan_fighterAT
+	class RB205_partisan_fighter_AA : RB205_partisan_fighter_AT
 	{
 		displayName = "Fighter (AA)";
 		uniformClass = "JMSLLTE_ScumPathfinder_brown_F_CombatUniform";
-		weapons[]=          { WEAPON_E5, WEAPON_AA, "Throw", "Put" };
-		respawnWeapons[]=   { WEAPON_E5, WEAPON_AA, "Throw", "Put" };
+		weapons[]=          { WEAPON_PRIMARY, WEAPON_AA, "Throw", "Put" };
+		respawnWeapons[]=   { WEAPON_PRIMARY, WEAPON_AA, "Throw", "Put" };
 		backpack = "RB205_partisan_fighter_backpack_AA";
-		magazines[]=        { MAG_8(MAG_E5), MAG_AA};
-		respawnMagazines[]= { MAG_8(MAG_E5), MAG_AA};
+		magazines[]=        { ENUM_8(MAG_PRIMARY), MAG_AA};
+		respawnMagazines[]= { ENUM_8(MAG_PRIMARY), MAG_AA};
 	};
 
 	class RB205_partisan_medic : RB205_partisan_squadLead
@@ -81,8 +83,23 @@ class CfgVehicles
 	class RB205_partisan_fighter_heavy : RB205_partisan_squadLead
 	{
 		displayName = "Heavy";
-		uniformClass = "JMSLLTE_ScumPart_yel_F_CombatUniform";
-		backpack = "RB205_partisan_fighter_backpack_heavy";
+		uniformClass = "JMSLLTE_ScumTroopHeavy_brown_F_CombatUniform";
+		linkedItems[]= {"JMSLLTE_HeavyBandBag_black_armor", "JMSLLTE_Commando_saw_helmet","ItemMap", "ItemCompass", "ItemWatch", "ItemRadio"};
+		respawnlinkedItems[]=   {"JMSLLTE_HeavyBandBag_black_armor", "JMSLLTE_Commando_saw_helmet","ItemMap", "ItemCompass", "ItemWatch", "ItemRadio"};
+		weapons[]= {WEAPON_HEAVY, "Throw", "Put" };
+		magazines[]=        { ENUM_2(MAG_HEAVY)};
+		respawnMagazines[]= { ENUM_2(MAG_HEAVY)};
+	};
+
+	class RB205_partisan_fighter_sniper : RB205_partisan_squadLead
+	{
+		displayName = "Sniper";
+		uniformClass = "JMSLLTE_ScumCommando_camo_F_CombatUniform";
+		linkedItems[]= {"JMSLLTE_HeavyBandBag_black_armor", "JMSLLTE_Commando_saw_helmet","ItemMap", "ItemCompass", "ItemWatch", "ItemRadio"};
+		respawnlinkedItems[]=   {"JMSLLTE_HeavyBandBag_black_armor", "JMSLLTE_Commando_saw_helmet","ItemMap", "ItemCompass", "ItemWatch", "ItemRadio"};
+		weapons[]= {WEAPON_SNIPER, "Throw", "Put" };
+		magazines[]=        { ENUM_4(MAG_SNIPER)};
+		respawnMagazines[]= { ENUM_4(MAG_SNIPER)};
 	};
 
 
@@ -108,21 +125,11 @@ class CfgVehicles
 	class RB205_partisan_fighter_backpack_medic : JMSLLTE_back_rebpack_v1_black
 	{
 		scope = 1;
-		class TransportMagazines
+		class TransportItems
 		{
-			MAG_XX(ACE_salineIV,2)
-			MAG_XX(ACE_salineIV_500,2)
-			MAG_XX(ACE_elasticBandage,20)
-		};
-	};
-
-	class JMSLLTE_back_rebpack_v1_wood;
-	class RB205_partisan_fighter_backpack_heavy : JMSLLTE_back_rebpack_v1_wood
-	{
-		scope = 1;
-		class TransportMagazines
-		{
-			MAG_XX(ACE_salineIV,2)
+			ITEM_XX(ACE_salineIV,2);
+			ITEM_XX(ACE_salineIV_500,2);
+			ITEM_XX(ACE_elasticBandage,20);
 		};
 	};
 
